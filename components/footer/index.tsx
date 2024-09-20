3cf094f07b9b9b0743bfea84649ac8aab6e032fe
Victoria <vmcnorrill@gmail.com>
2024-09-20 11:34:51 -0400

    footer links

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,11 +1,31 @@
 import "server-only";
 
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa6";
+
 const Footer = () => {
 	return (
-		<footer className='flex flex-row justify-center bottom-0 p-5'>
-			<p className='text-sm text-amber-700'>
+		<footer className='flex flex-row justify-between align-top bottom-0 p-5'>
+			<p className='text-sm text-amber-700 font-bold'>
 				&copy; {new Date().getFullYear()} Meowem Hissum. All rights reserved.
 			</p>
+			<div className='flex flex-row align-middle gap-5'>
+				<Link href='https://github.com/victoriamcn/meowem-hissum'>
+					<FaGithub className='text-amber-700' />
+				</Link>
+
+				<Link href='https://github.com/victoriamcn/meowem-hissum/issues/new/choose'>
+					<p className='text-sm text-amber-700 font-bold'>Contribute</p>
+				</Link>
+
+				<Link href='https://github.com/victoriamcn/meowem-hissum/issues/new/choose'>
+					<p className='text-sm text-amber-700 font-bold'>Request Feature</p>
+				</Link>
+
+				<Link href='https://github.com/victoriamcn/meowem-hissum/issues/new/choose'>
+					<p className='text-sm text-amber-700 font-bold'>Report Bug</p>
+				</Link>
+			</div>
 		</footer>
 	);
 };
