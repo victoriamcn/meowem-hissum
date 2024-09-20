a7d0d5a4abcf5b75aa454fa28f3439a438a30c7e
Victoria <vmcnorrill@gmail.com>
2024-09-20 16:23:21 -0400

    header and navigation. updated documentation. fixed typos. removed unused components.

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,31 +1,52 @@
 import "server-only";
 
 import Link from "next/link";
-import { FaGithub } from "react-icons/fa6";
+import { FaGithub, FaBug, FaCodePullRequest, FaCat } from "react-icons/fa6";
 
 const Footer = () => {
 	return (
-		<footer className='flex flex-row justify-between align-top bottom-0 p-5'>
-			<p className='text-sm text-amber-700 font-bold'>
-				&copy; {new Date().getFullYear()} Meowem Hissum. All rights reserved.
-			</p>
-			<div className='flex flex-row align-middle gap-5'>
-				<Link href='https://github.com/victoriamcn/meowem-hissum'>
-					<FaGithub className='text-amber-700' />
+		<footer className='flex flex-row justify-between items-center bottom-0 p-5 text-sm text-amber-700 font-semibold'>
+			<div className='flex flex-row gap-5 text-sm text-amber-700 font-semibold'>
+				<Link
+					href='https://github.com/victoriamcn/meowem-hissum'
+					className='flex flex-row align-middle gap-1 items-center'
+				>
+					<FaGithub size={13} />
+					<p>Github</p>
 				</Link>
 
-				<Link href='https://github.com/victoriamcn/meowem-hissum/issues/new/choose'>
-					<p className='text-sm text-amber-700 font-bold'>Contribute</p>
+				<Link
+					href='https://github.com/victoriamcn/meowem-hissum/issues/new/choose'
+					className='flex flex-row align-middle gap-1 items-center'
+				>
+					<FaCodePullRequest size={12} />
+					<p>Contribute</p>
 				</Link>
 
-				<Link href='https://github.com/victoriamcn/meowem-hissum/issues/new/choose'>
-					<p className='text-sm text-amber-700 font-bold'>Request Feature</p>
+				<Link
+					href='https://github.com/victoriamcn/meowem-hissum/issues/new/choose'
+					className='flex flex-row align-middle gap-1 items-center'
+				>
+					<FaCat size={13} />
+					<p>Request Feature</p>
 				</Link>
 
-				<Link href='https://github.com/victoriamcn/meowem-hissum/issues/new/choose'>
-					<p className='text-sm text-amber-700 font-bold'>Report Bug</p>
+				<Link
+					href='https://github.com/victoriamcn/meowem-hissum/issues/new/choose'
+					className='flex flex-row align-middle gap-1 items-center'
+				>
+					<FaBug size={12} />
+					<p>Report Bug</p>
 				</Link>
 			</div>
+
+			<Link href='https://www.britannica.com/topic/Lorem-ipsum'>
+				<p>What is Lorem ipsum?</p>
+			</Link>
+
+			<p>
+				&copy; {new Date().getFullYear()} Meowem Hissum. All rights reserved.
+			</p>
 		</footer>
 	);
 };
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -2,14 +2,16 @@ import "server-only";
 
 import Image from "next/image";
 import Link from "next/link";
+import Navigation from "@/components/navigation";
 import logo from "@/public/assets/meowem ipsum icon.png";
 
 export default function Header() {
 	return (
-		<header className='bg-amber-700 flex flex-row p-5'>
+		<header className=' sticky top-0 flex flex-row justify-between bg-amber-700 py-3 px-8 text-lg text-zinc-100 font-semibold'>
 			<Link href='/'>
-				<Image src={logo} alt='Meowem Ipsum logo' width={100} height={100} />
+				<Image src={logo} alt='Meowem Ipsum logo' width={65} height={65} />
 			</Link>
+			<Navigation />
 		</header>
 	);
 }
